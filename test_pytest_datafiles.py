8b89c9bdc4c1c694be3f2c05ab2960ffc20c323d
Omar Kohl <omarkohl@gmail.com>
2015-08-01 17:58:36 +0200

    Remove unnecessary level of indirection from tests

--- test_pytest_datafiles.py
+++ test_pytest_datafiles.py
@@ -1,74 +1,35 @@
-pytest_plugins = ['pytester']
+import pytest
 
 
-def test_copy_hosts(testdir):
+@pytest.mark.datafiles("/etc/hosts")
+def test_copy_hosts(datafiles):
     """
-    Create a test that relies on a 'hosts' file being present and that checks
-    if this file contains an entry 'localhost'.
-
-    Verify that that test runs sucessfully.
+    Verify that a 'hosts' file containing an entry 'localhost' has been copied
     """
-    testdir.makepyfile("""
-    import pytest
-
-    @pytest.mark.datafiles("/etc/hosts")
-    def test_file_copied(datafiles):
-        assert (datafiles / 'hosts').check(file=1)
-        assert 'localhost' in (datafiles / 'hosts').read_text('utf-8')
-    """)
-    result = testdir.runpytest()
-    assert result.ret == 0
-    assert result.errlines == []
-    result.stdout.fnmatch_lines([
-        "*1 passed*"
-        ])
+    assert (datafiles / 'hosts').check(file=1)
+    assert 'localhost' in (datafiles / 'hosts').read_text('utf-8')
 
 
-def test_multiple_files(testdir):
+@pytest.mark.datafiles("/etc/hosts", "/etc/hostname")
+def test_multiple_files(datafiles):
     """
-    Create a test that relies on several files being present.
-
-    Verify that that test runs sucessfully.
+    Verify multiple files are found
     """
-    testdir.makepyfile("""
-    import pytest
-
-    @pytest.mark.datafiles("/etc/hosts", "/etc/hostname")
-    def test_two_files_copied(datafiles):
-        assert (datafiles / 'hosts').check(file=1)
-        assert (datafiles / 'hostname').check(file=1)
-        assert len((datafiles).listdir()) == 2
-    """)
-    result = testdir.runpytest()
-    assert result.ret == 0
-    assert result.errlines == []
-    result.stdout.fnmatch_lines([
-        "*1 passed*"
-        ])
+    assert (datafiles / 'hosts').check(file=1)
+    assert (datafiles / 'hostname').check(file=1)
+    assert len((datafiles).listdir()) == 2
 
 
-def test_no_files(testdir):
+@pytest.mark.datafiles
+def test_no_files1(datafiles):
     """
-    Create a test that requires no files being copied before the test. Test
-    two cases:
-        1. datafiles marker is set but empty
-        2. datafiles marker is not set
-
-    Verify that those tests run sucessfully.
+    Verify if datafiles marker is set but empty the directory is empty
     """
-    testdir.makepyfile("""
-    import pytest
+    assert len((datafiles).listdir()) == 0
 
-    @pytest.mark.datafiles
-    def test_no_file1(datafiles):
-        assert len((datafiles).listdir()) == 0
 
-    def test_no_file2(datafiles):
-        assert len((datafiles).listdir()) == 0
-    """)
-    result = testdir.runpytest()
-    assert result.ret == 0
-    assert result.errlines == []
-    result.stdout.fnmatch_lines([
-        "*2 passed*"
-        ])
+def test_no_files2(datafiles):
+    """
+    Verify if datafiles marker is not set the directory is empty
+    """
+    assert len((datafiles).listdir()) == 0
