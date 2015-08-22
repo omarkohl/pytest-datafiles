ca24233c72a005e4406f9452cfa0a4690e41982e
Omar Kohl <omarkohl@gmail.com>
2015-08-22 19:26:12 +0200

    Add tests to demonstrate current handling of directories
    It is possible to specify directories instead of only files to be copied to the
    tmpdir. These tests show how this works.
    
    Some options to increase flexibility concerning directory handling will be
    implemented in future commits.

--- tests/test_pytest_datafiles.py
+++ tests/test_pytest_datafiles.py
@@ -78,3 +78,83 @@ def test_no_files2(datafiles):
     Verify if datafiles marker is not set the directory is empty
     """
     assert len((datafiles).listdir()) == 0
+
+
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'dir1'))
+def test_single_dir_str(datafiles):
+    """
+    Verify that a single directory (specified as a string) is copied correctly.
+
+    The content of the directory is copied.
+    """
+    assert len(datafiles.listdir()) == 3
+    assert (datafiles / 'file1').check(file=1)
+    assert (datafiles / 'file2').check(file=1)
+    assert (datafiles / 'file3').check(file=1)
+
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, 'dir1'),
+    os.path.join(FIXTURE_DIR, 'dir2'),
+    )
+def test_multi_dir_str(datafiles):
+    """
+    Verify that multiple directories (strings) are copied correctly.
+
+    The content of the directories is copied.
+    """
+    assert len(datafiles.listdir()) == 6
+    # files from dir1
+    assert (datafiles / 'file1').check(file=1)
+    assert (datafiles / 'file2').check(file=1)
+    assert (datafiles / 'file3').check(file=1)
+    # files from dir2
+    assert (datafiles / 'file4').check(file=1)
+    assert (datafiles / 'file5').check(file=1)
+    assert (datafiles / 'file6').check(file=1)
+
+
+@pytest.mark.datafiles(
+    path.local(FIXTURE_DIR) / 'dir1',
+    path.local(FIXTURE_DIR) / 'dir2',
+    path.local(FIXTURE_DIR) / 'dir3',
+    )
+def test_multi_dir_overwrite(datafiles):
+    """
+    Verify files with the same name are overwritten
+
+    When copying from multiple directories if files have the same name they are
+    overwritten by the last occurence.
+    """
+    assert len(datafiles.listdir()) == 6
+    # file1 and file4 appear twice and are overwritten by the one in dir3
+    assert (datafiles / 'file1').read() == "dir3\n123\n"
+    assert (datafiles / 'file2').read() == "dir1\n456\n"
+    assert (datafiles / 'file3').read() == "dir1\n789\n"
+    assert (datafiles / 'file4').read() == "dir3\n101112\n"
+    assert (datafiles / 'file5').read() == "dir2\n131415\n"
+    assert (datafiles / 'file6').read() == "dir2\n161718\n"
+
+
+@pytest.mark.datafiles(
+    path.local(FIXTURE_DIR) / 'dir1',
+    path.local(FIXTURE_FILES[0]),  # huckleberry.txt
+    path.local(FIXTURE_DIR) / 'dir4',
+    path.local(FIXTURE_FILES[1]),  # random.bin
+    )
+def test_multiple(datafiles):
+    """
+    Verify multiple files and directories (with subdirectories) are copied
+    """
+    assert len(datafiles.listdir()) == 7
+    assert (datafiles / 'huckleberry.txt').check(file=1)
+    assert (datafiles / 'random.bin').check(file=1)
+    assert (datafiles / 'file1').check(file=1)
+    assert (datafiles / 'file2').check(file=1)
+    assert (datafiles / 'file3').check(file=1)
+    assert (datafiles / 'subdir1').check(dir=1)
+    assert (datafiles / 'subdir2').check(dir=1)
+    assert (datafiles / 'subdir1' / 'file1').check(file=1)
+    assert (datafiles / 'subdir1' / 'file1').check(file=1)
+    assert (datafiles / 'subdir2' / 'file2').check(file=1)
+    assert (datafiles / 'subdir2' / 'file2').check(file=1)
